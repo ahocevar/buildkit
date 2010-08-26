86eacbd7b3bf62b8e99f46911dd5a633464b8570
tschaub <tim.schaub@gmail.com>
2010-08-26 16:12:04 -0600

    Updates to work with ringo.

--- tests/test_buildkit.js
+++ tests/test_buildkit.js
@@ -1,6 +1,5 @@
 exports["test: merge"] = require("./buildkit/test_merge");
 
-if (require.main == module) {
-    require("test/runner").run(exports);
+if (require.main == module || require.main == module.id) {
+    require("test").run(exports);
 }
-
